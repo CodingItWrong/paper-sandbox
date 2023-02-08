4ce759456b7a8ef93bf5245a4468bfb756f5e18b
Josh Justice <me@codingitwrong.com>
2023-02-08 06:23:55 -0500

    Add example text input

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { TextInput } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAwareScrollView>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <TextInput multiline label="My TextInput" />
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+      <Text>A</Text>
+    </KeyboardAwareScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
