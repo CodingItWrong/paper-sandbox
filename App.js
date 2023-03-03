8a229bb7ea25971f5ff0b5f222e91688447282f6
Josh Justice <me@codingitwrong.com>
2023-03-03 15:08:44 -0500

    Set up home screen

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, useColorScheme, View } from 'react-native';
+import { Provider as PaperProvider, Text, useTheme } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <PaperProvider>
+      <HomeScreen />
+    </PaperProvider>
+  );
+}
+
+function HomeScreen() {
+  const theme = useTheme();
+  const backgroundStyle = {backgroundColor: theme.colors.background};
+  return (
+    <View style={[styles.container, backgroundStyle]}>
+      <Text>Open up App.js to start working on your app!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +24,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
