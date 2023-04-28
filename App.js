4db11f95d161127bfdddd10016032d8f0793a7b3
Josh Justice <me@codingitwrong.com>
2023-04-28 07:19:14 -0400

    Set up demo menus

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {Provider as PaperProvider} from 'react-native-paper';
+import {Button, Menu, Provider as PaperProvider} from 'react-native-paper';
 
 export default function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <MenuAndButton label="Left Menu" />
+          <MenuAndButton label="Center Menu" />
+          <MenuAndButton label="Right Menu" />
+        </View>
       </View>
     </PaperProvider>
+  );
+}
+
+function MenuAndButton({label}) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <Menu
+      visible={menuOpen}
+      onDismiss={() => setMenuOpen(false)}
+      anchor={
+        <Button mode='contained' onPress={() => setMenuOpen(true)}>{label}</Button>
+      }
+    >
+      <Menu.Item title="Hi" onPress={() => {}} />
+    </Menu>
   );
 }
 
@@ -17,7 +39,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
   },
 });
